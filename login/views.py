c4ea39fdd357578fcc29c17e61e506abb1c6fdd5
acdiost <dawn@acdiost.com>
2020-03-03 16:36:59 +0800

    修复登录问题，解决主页样式排版异常

--- login/views.py
+++ login/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from . import models
 from . import forms
+
+
 # Create your views here.
 def index(request):
-    pass
     return render(request, 'login/index.html')
+
 
 # #新增 项目的管理菜单
 # def xmgl(request):
@@ -15,31 +17,30 @@ def index(request):
 def login(request):
     if request.session.get('is_login', None):  # 不允许重复登录
         return redirect('/index/')
+    if request.method == "GET":
+        login_form = forms.UserForm(request.POST)
+        return render(request, 'login/login.html', locals())
     if request.method == "POST":
         login_form = forms.UserForm(request.POST)
         message = '请检查填写的内容！'
         if login_form.is_valid():
             username = login_form.cleaned_data.get('username')
             password = login_form.cleaned_data.get('password')
-
             try:
                 user = models.User.objects.get(name=username)
             except:
-                message = '用户不存在！'
+                message = '用户名或密码错误！'
                 return render(request, 'login/login.html', locals())
-
             if user.password == password:
                 request.session['is_login'] = True
                 request.session['user_id'] = user.id
                 request.session['user_name'] = user.name
                 return redirect('/index/')
             else:
-                message = '密码不正确！'
+                message = '用户名或密码错误！'
                 return render(request, 'login/login.html', locals())
         else:
             return render(request, 'login/login.html', locals())
-
-    return render(request, 'login/login.html')
 
 
 def register(request):
